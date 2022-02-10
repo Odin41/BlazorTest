38df1e1f30fa756b218f20f11b594302d19772de
Odin41 <odin3476@gmail.com>
2022-02-10 16:18:12 +0400

    Blazor tooltipTest

---
+++ BlazorTest/wwwroot/BlazorTest.lib.module.js
@@ -0,0 +1,11 @@
+﻿export function beforeStart(options, extensions) {
+    console.log("beforeStart");
+}
+
+export function afterStarted(blazor) {
+    console.log("afterStarted");
+    //$(document).ready(() => {
+        $('[data-bs-toggle="tooltip"]').tooltip();
+        $('[data-toggle="tooltip"]').tooltip();
+    //});
+}
